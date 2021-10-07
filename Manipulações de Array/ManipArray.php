3665e81da602d6fab9b924f0cbd3fa3c91550529
henriquealansantoss <henriquealansantoss@gmail.com>
2021-10-07 09:38:21 -0300

    Manipulando Array - slice, splice, array_reduce e list

--- Manipulações de Array/ManipArray.php
+++ Manipulações de Array/ManipArray.php
@@ -65,3 +65,50 @@ echo '<br>';
 echo 'array_values' . '<br>';
 $values = array_values($array2);
 print_r($values); // retorna um novo array com o valores sem as chaves originais.
+
+echo '<br>' . '--------------' . '<br>';
+
+echo 'array_slice' . '<br>';
+
+$retorno = array_slice($array2, 2, 3);
+print_r($retorno);
+
+echo '<br>' . '--------------' . '<br>';
+
+echo 'array_splice' . '<br>';
+// remover ou substituir uma valor de um array
+
+array_splice($array2, 2, 1);
+print_r($array2);
+echo '<br>';
+
+array_splice($array2, 0, 1, 'Nome adicionado');
+print_r($array2);
+echo '<br>';
+
+array_splice($array2, 2, 2, ['Nome1', 'Nome2']);
+print_r($array2);
+
+echo '<br>' . '--------------' . '<br>';
+
+echo 'array_reduce' . '<br>';
+
+$numeros = [2, 3, 4, 5, 6];
+
+$result = array_reduce($numeros, function ($sub, $item) {
+    $sub += $item;
+    return $sub;
+}, 0);
+
+echo $result;
+
+echo '<br>' . '--------------' . '<br>';
+
+echo 'list' . '<br>';
+
+$arrayPessoa = ['Henrique', 29, 'preto', '80'];
+
+list($nome, $idade, $cor, $quilos) = $arrayPessoa;
+
+echo "Meu nome é $nome, tenho $idade anos e peso $quilos kgs";
+
